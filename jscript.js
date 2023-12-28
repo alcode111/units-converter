b442a25f4e281f3ad7f39de273ba8f2489852e2f
Ismail <141173099+alcode111@users.noreply.github.com>
2023-12-28 10:36:21 +0100

    Add conversion factors in constants, the first function and an event listener

--- jscript.js
+++ jscript.js
@@ -6,3 +6,24 @@ const paraLitersGallons = document.getElementById("liters-gallons")
 const paraKilosPounds = document.getElementById("kilos-pounds")
 
 let userInput = ""
+
+const meterToFoot = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
+
+convertBtn.addEventListener('click', function() {
+    userInput = unitInput.value
+    unitInput.value = ""
+
+    paraMeterFeet.innerHTML = `<p class="measure-subtitle" id="meters-feet">
+    ${userInput} meters = ${metersToFeet(userInput).toFeet} feet | ${userInput} feet = ${metersToFeet(userInput).toMeters} meters
+    </p>`
+
+})
+
+function metersToFeet(meter){
+    return {
+        toFeet: (meter * meterToFoot).toFixed(3),
+        toMeters: (meter / meterToFoot).toFixed(3)
+    }
+}
