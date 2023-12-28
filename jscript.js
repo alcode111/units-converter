e946bb07d512330580c1dbd62eb21355f376809c
Ismail <141173099+alcode111@users.noreply.github.com>
2023-12-28 10:00:32 +0100

    Add DOM elements and initialize variable for user input

---
+++ jscript.js
@@ -0,0 +1,8 @@
+const unitInput = document.getElementById("unit-input")
+const convertBtn = document.getElementById("convert-btn")
+
+const paraMeterFeet = document.getElementById("meters-feet")
+const paraLitersGallons = document.getElementById("liters-gallons")
+const paraKilosPounds = document.getElementById("kilos-pounds")
+
+let userInput = ""
